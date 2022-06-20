e70b51cb76674aa2b33eff49ce89c27c12eef42d
Brian Perry <blperry5@gmail.com>
2022-06-19 23:59:23 -0700

    Added Nav Elements

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<nav>
+	<div class="nav__personal">
+		<a class="name" href="/">Brian Perry</a>
+		<a class="about" href="/">About Me</a>
+	</div>
+
+	<div class="nav__links">
+		<a href="/">Services</a>
+		<a href="/">Portfolio</a>
+		<a href="/">Blog</a>
+	</div>
+	<div class="nav__socials">
+		<a href="/" class="">LinkedIn</a>
+		<a href="/" class="">Email</a>
+		<a href="/" class="">GitHub</a>
+	</div>
+</nav>
+
+<slot />
+
+<style lang="scss">
+	nav {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
